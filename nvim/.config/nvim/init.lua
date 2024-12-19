94834a5d78351f1e03661fca0896ad1a64189723
Devan Goodwin <dgoodwin@rm-rf.ca>
2024-12-19 13:27:49 -0400

    Start with Telescope fuzzy finding

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -73,6 +73,12 @@ require("lazy").setup({
       end,
     },
 
+    -- Telescope for fuzzy finding
+    {
+      'nvim-telescope/telescope.nvim', branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    }
+
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
@@ -80,3 +86,11 @@ require("lazy").setup({
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fgb', builtin.git_branches, { desc = 'Telescope git branches' })
+vim.keymap.set('n', '<leader>fgs', builtin.git_status, { desc = 'Telescope git status' })
